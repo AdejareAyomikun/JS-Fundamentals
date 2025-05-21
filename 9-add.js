acde973708301c8397cba18ee6ff7d8b0d419312
Adejare Faruk <nou183023868@noun.edu.ng>
2025-05-21 23:06:14 +0100

    update

--- 9-add.js
+++ 9-add.js
@@ -6,7 +6,7 @@ const num1 = parseInt(process.argv[2]);
 const num2 = parseInt(process.argv[3]);
 
 if (isNaN(num1) || isNaN(num2)) {
-    console.log("Please provide two integers");
+    console.log("NaN");
 } else {
     console.log(add(num1, num2));
 }
